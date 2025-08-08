b9df4e581cf747c4ccffa3ded49c153e7cbf4cc1
rtkay123 <dev@kanjala.com>
2025-08-08 08:20:46 +0200

    feat(warden): config file

--- crates/warden/src/main.rs
+++ crates/warden/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{Parser, command};
+use stack_up::{Configuration, tracing::Tracing};
+use tracing::info;
+
+/// warden
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to config file
+    #[arg(short, long)]
+    config_file: Option<std::path::PathBuf>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    let config = include_str!("../warden.toml");
+
+    let mut config = config::Config::builder()
+        .add_source(config::File::from_str(config, config::FileFormat::Toml));
+
+    if let Some(cf) = args.config_file.as_ref().and_then(|v| v.to_str()) {
+        config = config.add_source(config::File::new(cf, config::FileFormat::Toml));
+    };
+
+    let mut config: Configuration = config.build()?.try_deserialize()?;
+    config.application.name = env!("CARGO_CRATE_NAME").into();
+    config.application.version = env!("CARGO_PKG_VERSION").into();
+
+    let _tracing = Tracing::builder().build(&config.monitoring);
+
+    info!("Hello, world!");
+    Ok(())
 }
