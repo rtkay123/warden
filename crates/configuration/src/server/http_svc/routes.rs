4a9e22a58879e16cc2f53cf29abff0ca69d13a5e
rtkay123 <dev@kanjala.com>
2025-08-14 18:39:26 +0200

    fix(config): routes nesting

--- crates/configuration/src/server/http_svc/routes.rs
+++ crates/configuration/src/server/http_svc/routes.rs
@@ -13,6 +13,8 @@ pub fn router(store: AppHandle) -> OpenApiRouter {
             routing::post_routing::post_routing,
             routing::delete_routing::delete,
             routing::replace_routing::replace,
+        ))
+        .routes(routes!(
             /* rule */
             rule::create::create_rule,
             rule::update::update_rule_config,
