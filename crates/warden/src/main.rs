03f0ae88e7c7cb59e31f9f2577491d532f9819a9
rtkay123 <dev@kanjala.com>
2025-08-18 22:59:07 +0200

    clippy

--- crates/warden/src/main.rs
+++ crates/warden/src/main.rs
@@ -62,6 +62,9 @@ async fn main() -> Result<(), error::AppError> {
         .inspect_err(|e| error!("nats: {e}"))?
         .build();
 
+    let mut more = vec![];
+    more.push(2);
+
     let postgres = services
         .postgres
         .take()
