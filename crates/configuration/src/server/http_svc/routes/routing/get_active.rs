5ea055f7c8b51d1f828e5ceac8b4d048adb1c956
rtkay123 <dev@kanjala.com>
2025-08-19 20:21:03 +0200

    test(config): post routing

--- crates/configuration/src/server/http_svc/routes/routing/get_active.rs
+++ crates/configuration/src/server/http_svc/routes/routing/get_active.rs
@@ -37,3 +37,82 @@ pub async fn active_routing(
         .into_inner();
     Ok(axum::Json(config.configuration).into_response())
 }
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use sqlx::PgPool;
+    use tower::ServiceExt;
+    use warden_stack::cache::RedisManager;
+
+    use crate::{
+        server::http_svc::{build_router, routes::test_config},
+        state::{AppState, Services},
+    };
+
+    #[sqlx::test]
+    async fn post(pool: PgPool) {
+        let config = test_config();
+
+        let cache = RedisManager::new(&config.cache).await.unwrap();
+        let client = async_nats::connect(&config.nats.hosts[0]).await.unwrap();
+        let jetstream = async_nats::jetstream::new(client);
+
+        let state = AppState::create(
+            Services {
+                postgres: pool,
+                cache,
+                jetstream,
+            },
+            &test_config(),
+        )
+        .await
+        .unwrap();
+        let app = build_router(state);
+
+        let routing = serde_json::json!(
+        {
+            "active": true,
+            "name": "Public Network Map",
+            "version": "1.0.0",
+            "messages": [
+                {
+                    "id": "004",
+                    "version": "1.0.0",
+                    "tx_tp": "pacs.002.001.12",
+                    "typologies": [
+                    {
+                        "id": "999",
+                        "version": "1.0.0",
+                        "rules": [
+                        {
+                            "id": "901",
+                            "version": "1.0.0"
+                        }
+                        ]
+                    }
+                    ]
+                }
+            ]
+        });
+
+        let body = serde_json::to_vec(&routing).unwrap();
+
+        let response = app
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .method("POST")
+                    .header("Content-Type", "application/json")
+                    .uri("/api/v0/routing")
+                    .body(Body::from(body))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::CREATED);
+    }
+}
